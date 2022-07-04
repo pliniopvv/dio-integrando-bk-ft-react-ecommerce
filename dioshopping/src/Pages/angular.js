de7b8f58c46583746ae083ef8fca04a79a4b56a9
pliniopvv <gangss2@Hotmail.com>
2022-07-04 11:46:59 -0300

    finish

--- dioshopping/src/Pages/angular.js
+++ dioshopping/src/Pages/angular.js
@@ -1,9 +1,23 @@
+import { makeStyles } from "@material-ui/core";
 import { Fragment } from "react";
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         flexGrow: 1,
+//         marginTop: '5px',
+//     },
+//     paper: {
+//         padding: '5px',
+//     }
+// }));
 
 const AngularFramework = () => {
     return (
         <Fragment>
-            Angular é muito melhor!
+            <div style={{width: '50vw', margin: 'auto'}}>
+                <p>Angular é melhor!</p>
+                <p>Baixe-o executando o comando: <span style={{padding: '3px', backgroundColor: 'gray'}}>npm install -g @angular/cli</span></p>
+            </div>
         </Fragment>
     );
 }
